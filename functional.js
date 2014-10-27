f9d90b2a03675c606ed541146ec82ba875e97ba0
Mel Quark <mel.quark@gmail.com>
2014-10-27 14:41:25 -0700

    added exercises

---
+++ functional.js
@@ -0,0 +1,34 @@
+var exercises = {};
+
+// 4 points
+// this function takes a variable number of string arguments
+// use the arguments keyword as well as the _.each() method from
+// lodash to return the first string containing the substring
+// "waldo"
+exercises.wheresWaldo = function(){
+  // TODO: implement me
+}
+
+// 4 points
+// given an array of arrays (all holding numbers), return an
+// array holding the largest number in each sub-array.
+// use _.map() and _.max()
+exercises.largestNums = function(arrayOfNumberArrays){
+  // TODO: implement me
+}
+
+// 4 points
+// you are given an array of Date objects and two boundary dates
+// use the lodash _.filter() method to return an array with all
+// the objects from dates that fit within the given boundaries
+exercises.filterDates = function(dates, lowerBound, upperBound){
+  // TODO: implement me
+}
+
+// 8 points (including the test)
+// make up your own exercise like those above. implement it and
+// write a test for it in tests/functional.spec.js.
+// exercises.TODO = function(array){
+// }
+
+module.exports = exercises;
---
+++ tests/functional.spec.js
@@ -0,0 +1,10 @@
+var expect = require("../node_modules/chai/chai").expect;
+var functional = require("../functional");
+
+describe("Functional", function(){
+
+  it("should ", function(){
+  });
+
+});
+
