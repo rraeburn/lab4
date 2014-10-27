e3cb141f1efade805f3e6e2b6dc20b6ed4a3a41d
Mel Quark <mel.quark@gmail.com>
2014-10-27 16:06:29 -0700

    add tests

--- tests/functional.spec.js
+++ tests/functional.spec.js
@@ -1,10 +1,47 @@
-var expect = require("../node_modules/chai/chai").expect;
-var functional = require("../exercises/functional");
+var expect = require("../node_modules/chai/chai").expect,
+    functional = require("../exercises/functional");
 
-describe("Functional", function(){
+describe("Functional", function() {
 
-  it("should ", function(){
+  it("should find waldo", function() {
+    var result = functional.wheresWaldo(
+      "kitty star trek", "firefly rules",
+      "waldorf salad", "read dune"
+    );
+
+    // use equal to compare primitives
+    expect(result).to.equal("waldorf salad");
   });
 
-});
+  it("should return filtered dates", function() {
+    var dateArray = [],
+        lower = new Date("2001/01/01"),
+        upper = new Date("2004/01/01"),
+        answer, result;
 
+    // create test data
+    dateArray.push( new Date("2000/12/1") );
+    dateArray.push( new Date("2001/12/1") );
+    dateArray.push( new Date("2002/12/1") );
+    dateArray.push( new Date("2003/12/1") );
+
+    // expected result
+    answer = [ dateArray[1], dateArray[2], dateArray[3] ];
+
+    // call the method we're testing
+    result = functional.filterDates(dateArray, lower, upper);
+
+    expect(result).to.eql(answer);
+  });
+
+  it("should find the largest numbers", function() {
+    var testData = [ [ 1, 2 ],  [ 4, 10 ],  [ 15, 1, 3 ],  [ 0 ] ],
+        result = functional.largestNums(testData);
+
+    // use eql to compare objects
+    expect(result).to.eql([ 2, 10, 15, 0 ]);
+  });
+
+  it("should test the exercise you wrote yourself");
+
+});
