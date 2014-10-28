4a9bee77e5a690f27c637efc9e78875025d9becf
Mel Quark <mel.quark@gmail.com>
2014-10-28 02:11:35 -0700

    update points

--- exercises/functional.js
+++ exercises/functional.js
@@ -1,6 +1,6 @@
 var exercises = {};
 
-// 4 points
+// 8 points
 // this function takes a variable number of string arguments
 // use the arguments keyword as well as the _.each() method from
 // lodash to return the first string containing the substring
@@ -9,15 +9,15 @@ exercises.wheresWaldo = function() {
   // TODO: implement me
 };
 
-// 4 points
+// 8 points
 // given an array of arrays (all holding numbers), return an
 // array holding the largest number in each sub-array.
-// use _.map() and _.max()
+// use _.chain() and _.map() and _.max()
 exercises.largestNums = function(arrayOfNumberArrays) {
   // TODO: implement me
 };
 
-// 4 points
+// 8 points
 // you are given an array of Date objects and two boundary dates
 // use the lodash _.filter() method to return an array with all
 // the objects from dates that fit within the given boundaries
@@ -25,10 +25,10 @@ exercises.filterDates = function(dates, lowerBound, upperBound) {
   // TODO: implement me
 };
 
-// 8 points (including the test)
+// 16 points (including the test)
 // make up your own exercise like those above. implement it and
 // write a test for it in tests/functional.spec.js.
-// for 2 extra credit points, use _.reduce()
+// for 4 extra credit points, use _.reduce()
 // exercises.TODO = function(array) {
 // };
 
